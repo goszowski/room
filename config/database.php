d3fda384fed07079664211617538e4ada3f977d7
Jaroslaw Goszowski <goszowski@gmail.com>
2018-01-04 14:16:50 +0200

    DB config

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'dev.runsite.com.ua'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'room'),
+            'username' => env('DB_USERNAME', 'developer'),
+            'password' => env('DB_PASSWORD', 'altertable025'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
